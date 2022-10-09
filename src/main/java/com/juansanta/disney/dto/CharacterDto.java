390658e7a06c4592462c2dcf34f3aac0b3b861b1
Juan Santa <jcsantahurtado@gmail.com>
2022-10-09 18:17:08 -0500

    adding @NoArgsConstructor notation

--- src/main/java/com/juansanta/disney/dto/CharacterDto.java
+++ src/main/java/com/juansanta/disney/dto/CharacterDto.java
@@ -2,11 +2,13 @@ package com.juansanta.disney.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class CharacterDto {
 
